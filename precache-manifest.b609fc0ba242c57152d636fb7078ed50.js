a8571aa3ec865f7f20536a909a2c69c6ecf637ea
Communication Style Inventory <Communication.Style.Inventory@protonmail.com>
2020-02-15 20:52:07 -0800

    Updates

---
+++ precache-manifest.b609fc0ba242c57152d636fb7078ed50.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88cc401de3bcc5f07e94161e6ff9d0e7",
+    "url": "/Communication-Style-Inventory/index.html"
+  },
+  {
+    "revision": "b5e12686c807b22f5bc1",
+    "url": "/Communication-Style-Inventory/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "3227ab6c0539126666a6",
+    "url": "/Communication-Style-Inventory/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b5e12686c807b22f5bc1",
+    "url": "/Communication-Style-Inventory/static/js/2.c056e600.chunk.js"
+  },
+  {
+    "revision": "e933f8dbfdf496cb72e495663a499a00",
+    "url": "/Communication-Style-Inventory/static/js/2.c056e600.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3227ab6c0539126666a6",
+    "url": "/Communication-Style-Inventory/static/js/main.12a24fd6.chunk.js"
+  },
+  {
+    "revision": "efb44237186bcc5e52a4",
+    "url": "/Communication-Style-Inventory/static/js/runtime-main.f9879129.js"
+  }
+]);
